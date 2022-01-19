2bad36a8d88bec61baa712510479ebe807a2e9df
Hari Shankar <harishankar.official@gmail.com>
2022-01-19 23:54:02 +0530

    first

--- practicePython/characterInput.py
+++ practicePython/characterInput.py
@@ -1,1 +1,10 @@
-print("hello navaneeth")
+age = int(input("enter your age"))
+num = int(input("enter the number of times you want the message to be repeated"))
+
+year = 2022
+
+#year_100 = year + (100 - age)
+
+
+for n in range(num):
+    print(f'you will be 100 in {year + (100 - age)}', end="")
